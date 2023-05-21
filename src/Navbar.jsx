7c3c03d1d839a2ff6c00a0e86b5fabae7fc60916
bppalomer <bppalomer@gmail.com>
2023-05-21 14:50:47 +0800

    Added the navbar on the site

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,60 +1,42 @@
-function Navbar () {
-    return (
-        <nav class="navbar navbar-expand-lg">
-        <div class="container-fluid">
-          <a class="navbar-brand mt-2 ms-4" href="test.html"
-            ><p id="logo1">
-              Sp<img
-                src="images/Chili_logo.png"
-                alt="chili_image"
-                class="logo_image"
-              />ce<span id="logo2">Savvy</span>
-            </p></a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarScroll"
-            aria-controls="navbarScroll"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarScroll">
-            <ul
-              class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll"
-              style="--bs-scroll-height: 100px"
-            >
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a
-                  class="nav-link dropdown-toggle"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Recipe
-                </a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Chicken</a></li>
-                  <li><hr class="dropdown-divider" /></li>
-                  <li><a class="dropdown-item" href="#">Beef</a></li>
-                  <li><hr class="dropdown-divider" /></li>
-                  <li><a class="dropdown-item" href="#">Pork</a></li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link">About Us</a>
-              </li>
-            </ul>
-          </div>
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#"><p id="logo1" className ="mt-2 ms-3">Sp<img src="/Chili_logo.png" alt="" className="logo_image"/>ce <span id="logo2">Savvy</span></p></a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Category
+              </a>
+              <ul className="dropdown-menu dropdown-menu-end">
+                <li><a className="dropdown-item" href="#">Beef</a></li>
+                <li><hr className="dropdown-divider" /></li>
+                <li><a className="dropdown-item" href="#">Chicken</a></li>
+                <li><hr className="dropdown-divider" /></li>
+                <li><a className="dropdown-item" href="#">Pork</a></li>
+                <li><hr className="dropdown-divider" /></li>
+                <li><a className="dropdown-item" href="#">Fish</a></li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">About Us</a>
+            </li>
+
+          </ul>
         </div>
-      </nav>
-    )    
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
