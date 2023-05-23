7361680d93ec46070d65c7b2ff21e065b0d3bd71
xEliezer <layugeliezerbongco16@gmail.com>
2023-05-23 22:53:18 +0800

    added content on home page, bg, minor changes and edits on footer and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { useEffect } from 'react'
 import Index from './Pages/Index'
 import './Pages/Css/Style.css'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 
 
 function App () {
+
+    useEffect(()=>{
+        AOS.init();
+      }, []);
+
     return (
         <>
         <Index />
--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -4,39 +4,39 @@ const Navbar = () => {
   const find = useLocation();
   return (
     <>
-      <nav className="navbar navbar-expand-lg">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#"><span id="logo1">Sp<img src="/Chili_logo.png" alt="" className="logo_image"/>ce </span><span id="logo2">Savvy</span></a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-        <Link to="/" className={`nav-link ${ (find.pathname === `/`) ? `fw-bold active`: ``}`}>Home</Link>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Recipe
-          </a>
-          <ul className="dropdown-menu">
-            <li><Link to="/porklists" className={`nav-link nav_link ${ (find.pathname === `/porklists`) ? `fw-bold active`: ``}`}>Pork</Link></li>
-            <li><hr class="dropdown-divider" /></li>
-            <li><Link to="/beeflists" className={`nav-link nav_link ${ (find.pathname === `/beeflists`) ? `fw-bold active`: ``}`}>Beef</Link></li>
-            <li><hr class="dropdown-divider" /></li>
-            <li><Link to="/chickenlists" className={`nav-link nav_link ${ (find.pathname === `/chickenlists`) ? `fw-bold active`: ``}`}>Chicken</Link></li>
-            <li><hr class="dropdown-divider" /></li>
-            <li><Link to="/fishlists" className={`nav-link nav_link ${ (find.pathname === `/fishlists`) ? `fw-bold active`: ``}`}>Fish</Link></li>
-          </ul>
-        </li>
-        <li className="nav-item">
-        <Link to="/aboutus" className={`nav-link ${ (find.pathname === `/aboutus`) ? `fw-bold active`: ``}`}>About</Link>
-        </li>
-        
-      </ul>
-    </div>
-  </div>
-</nav>
+      <nav className="navbar navbar-expand-lg p-4">
+        <div className="container-fluid">
+          <a className="navbar-brand ms-4" href="#"><span id="logo1">Sp<img src="/Chili_logo.png" alt="" className="logo_image" />ce </span><span id="logo2">Savvy</span></a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link to="/" className={`nav-link ${(find.pathname === `/`) ? `fw-bold active` : ``}`}>Home</Link>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Recipe
+                </a>
+                <ul className="dropdown-menu">
+                  <li><Link to="/porklists" className={`nav-link nav_link ${(find.pathname === `/porklists`) ? `fw-bold active` : ``}`}>Pork</Link></li>
+                  <li><hr className="dropdown-divider" /></li>
+                  <li><Link to="/beeflists" className={`nav-link nav_link ${(find.pathname === `/beeflists`) ? `fw-bold active` : ``}`}>Beef</Link></li>
+                  <li><hr className="dropdown-divider" /></li>
+                  <li><Link to="/chickenlists" className={`nav-link nav_link ${(find.pathname === `/chickenlists`) ? `fw-bold active` : ``}`}>Chicken</Link></li>
+                  <li><hr className="dropdown-divider" /></li>
+                  <li><Link to="/fishlists" className={`nav-link nav_link ${(find.pathname === `/fishlists`) ? `fw-bold active` : ``}`}>Fish</Link></li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <Link to="/aboutus" className={`nav-link ${(find.pathname === `/aboutus`) ? `fw-bold active` : ``}`}>About</Link>
+              </li>
+
+            </ul>
+          </div>
+        </div>
+      </nav>
     </>
   );
 }
