eb874b21c98e000fa4ee794fd35fd62c872ecc25
Marjmainit <marjoriemainit.28@gmail.com>
2023-05-27 20:59:26 +0800

    align contact  page

--- src/Contactuspage.jsx
+++ src/Contactuspage.jsx
@@ -4,13 +4,13 @@ const Contactuspage = () => {
   return (
     <>
       <section className='container p-5 m-5 '>
-        <form>
+        <form className='contact'>
           <div>
             <h1 id='text'>Contact Us</h1>
             <p className='text' >We'd love to hear from you! If you have any questions, suggestions, or simply want to say hello,
               feel free to get in touch with us. Please fill out the form below, and we'll get back to you as soon as possible.</p>
           </div>
-          <div className='row pt-5'>
+          <div className='row pt-5 me-5 d-flex justify-content-center'>
             <div className="mb-3 ms-5 col-md-5">
               <label for="text" className="form-label">First Name</label>
               <input type="text" className="form-control" id="firstname" />
@@ -27,7 +27,7 @@ const Contactuspage = () => {
             <label for="text" className="form-label">Comment</label>
               <textarea type="text" className="form-control" id="Comment" rows="3" />
             </div>
-            <button type="submit" className="btn btn-primary bg-info col-md-2 m-5">Submit</button>
+            <button type="submit" className="btn btn-primary bg-info col-md-2 m-5">Enter</button>
           </div>
         </form>
       </section>
