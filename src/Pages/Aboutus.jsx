fe0aa8fe99272a0f1c928bf9b6862d5c97464f9b
Patrick <dalepatrick005@gmail.com>
2023-05-27 11:21:15 +0800

    added cotnten

--- src/Pages/Aboutus.jsx
+++ src/Pages/Aboutus.jsx
@@ -1,7 +1,47 @@
 
 function Aboutus () {
     return (
-        <h1>This is the Aboutus page</h1>
+        <section>
+            <div class="row" id="section">
+                <div class="col-md-5 container-fluidp p-5 m-2">
+                    <img src="#" alt="#"/>
+                </div>
+                <div class="col-md-5 container-fluid p-3 m-5">
+                    <p class="fw-bold">Marites Sulayman</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti maxime porro placeat optio
+                        repellendus, eius expedita fugiat quibusdam assumenda saepe quia libero dicta dolores doloremque
+                        error obcaecati totam necessitatibus itaque.</p>
+                </div>
+            </div>
+
+            <br />
+
+            <div class="row" id="section_2">
+                <div class="col-md-5 container-fluid p-3 m-2">
+                    <p class="fw-bold">Baldy dulingan</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti maxime porro placeat optio
+                        repellendus, eius expedita fugiat quibusdam assumenda saepe quia libero dicta dolores doloremque
+                        error obcaecati totam necessitatibus itaque.</p>
+                </div>
+                <div class="col-md-5 container-fluid p-5 m-2">
+                    <img src="#" alt="#"/>
+                </div>
+            </div>
+
+            <br />
+
+            <div class="row" id="section_3">
+                <div class="col-md-5 container-fluid p-3 m-2">
+                    <img src="#" alt="#" />
+                </div>
+                <div class="col-md-5 container-fluid p-3 m-5">
+                    <p class="fw-bold">Marites Sulayman</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti maxime porro placeat optio
+                        repellendus, eius expedita fugiat quibusdam assumenda saepe quia libero dicta dolores doloremque
+                        error obcaecati totam necessitatibus itaque.</p>
+                </div>
+            </div>
+        </section>
     )
 }
 
