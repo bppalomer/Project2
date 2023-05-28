bb266274c5e68be36a8b469c76d5e2809d2fe1c7
bppalomer <bppalomer@gmail.com>
2023-05-28 15:12:55 +0800

    added cards and filter on the recipe page

--- src/Pages/Recipepage.jsx
+++ src/Pages/Recipepage.jsx
@@ -1,55 +1,117 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Recipepage() {
   const [isOpen, setIsOpen] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedFilters, setSelectedFilters] = useState([]);
+  const [filteredRecipes, setFilteredRecipes] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [recipes, setRecipes] = useState([]);
+  const [isSearching, setIsSearching] = useState(false); // New state for tracking search status
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      let response;
+      if (selectedFilters.length === 0) {
+        response = await fetch(
+          "https://www.themealdb.com/api/json/v1/1/random.php"
+        );
+      } else {
+        const query = selectedFilters
+          .map((filter) => `&${filter.category}=${filter.value}`)
+          .join("");
+        response = await fetch(
+          `https://www.themealdb.com/api/json/v1/1/filter.php?${query}`
+        );
+      }
+      const data = await response.json();
+      setRecipes(data.meals);
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, [selectedFilters]);
+
+  useEffect(() => {
+    // Filter recipes based on search query
+    const filtered = recipes.filter((recipe) =>
+      recipe.strMeal.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setFilteredRecipes(filtered);
+  }, [recipes, searchQuery]);
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearchQuery("");
+    setIsSearching(true);
+  };
+
+  const handleCheckboxChange = (e) => {
+    const value = e.target.value;
+    const category = e.target.getAttribute("data-category");
+    if (e.target.checked) {
+      setSelectedFilters((prevFilters) => [...prevFilters, { category, value }]);
+    } else {
+      setSelectedFilters((prevFilters) =>
+        prevFilters.filter(
+          (filter) => !(filter.category === category && filter.value === value)
+        )
+      );
+    }
+  };
+
   return (
     <>
-      <section className="containe-fluid">
+      <section className="container-fluid p-0">
         <div className="search_section p-5 d-flex flex-column align-items-center">
           <div className="input-group justify-content-center mt-4">
-            <form action="" method="GET" className="form">
+            <form action="" method="GET" className="form" onSubmit={handleSearch}>
               <input
                 type="search"
                 placeholder="Search recipe ..."
                 className="search-field"
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
               />
               <button type="submit" className="search-button">
                 <i className="fa-solid fa-magnifying-glass"></i>
               </button>
             </form>
           </div>
-          <div className="mt-3">
-          
-              <span
-                className={`arrow ${isOpen ? "rotate" : ""} text-light`}
-                onClick={toggleDropdown}
-              >
-                {isOpen ? "Filter by category ▶" : "Filter by category ▼"}
-              </span>
+          <div className="mt-3 mb-4">
+            <span
+              className={`arrow ${isOpen ? "rotate" : ""} text-light`}
+              onClick={toggleDropdown}
+            >
+              {isOpen ? "Filter by category ▼" : "Filter by category ▶"}
+            </span>
           </div>
         </div>
         <div>
           {isOpen && (
-            <div className="dropdown-content container-fluid">
-              <div className="row">
+            <div className="dropdown-content">
+              <div className="row mx-auto justify-content-center">
                 <div className="filter_section col-md-2 p-3 m-4 border rounded">
                   <div>
-                    <h3 className="text-light">Meat Recipe</h3>
+                    <h3 className="text-light">Meat</h3>
                   </div>
                   <div className="form-check">
                     <input
                       className="form-check-input"
                       type="checkbox"
-                      value=""
-                      id="flexCheckDefault"
+                      value="Beef"
+                      data-category="c"
+                      id="flexCheckDefault1"
+                      onChange={handleCheckboxChange}
                     />
                     <label
                       className="form-check-label text-light"
-                      htmlFor="flexCheckDefault"
+                      htmlFor="flexCheckDefault1"
                     >
                       Beef
                     </label>
@@ -58,12 +120,14 @@ function Recipepage() {
                     <input
                       className="form-check-input"
                       type="checkbox"
-                      value=""
-                      id="flexCheckChecked"
+                      value="Chicken"
+                      data-category="c"
+                      id="flexCheckDefault2"
+                      onChange={handleCheckboxChange}
                     />
                     <label
                       className="form-check-label text-light"
-                      htmlFor="flexCheckChecked"
+                      htmlFor="flexCheckDefault2"
                     >
                       Chicken
                     </label>
@@ -72,12 +136,14 @@ function Recipepage() {
                     <input
                       className="form-check-input"
                       type="checkbox"
-                      value=""
-                      id="flexCheckDefault"
+                      value="Seafood"
+                      data-category="c"
+                      id="flexCheckDefault3"
+                      onChange={handleCheckboxChange}
                     />
                     <label
                       className="form-check-label text-light"
-                      htmlFor="flexCheckDefault"
+                      htmlFor="flexCheckDefault3"
                     >
                       Seafood
                     </label>
@@ -86,12 +152,14 @@ function Recipepage() {
                     <input
                       className="form-check-input"
                       type="checkbox"
-                      value=""
-                      id="flexCheckDefault"
+                      value="Pork"
+                      data-category="c"
+                      id="flexCheckDefault4"
+                      onChange={handleCheckboxChange}
                     />
                     <label
                       className="form-check-label text-light"
-                      htmlFor="flexCheckDefault"
+                      htmlFor="flexCheckDefault4"
                     >
                       Pork
                     </label>
@@ -105,26 +173,30 @@ function Recipepage() {
                     <input
                       className="form-check-input"
                       type="checkbox"
-                      value=""
-                      id="flexCheckDefault"
+                      value="Canadian"
+                      data-category="a"
+                      id="flexCheckDefault5"
+                      onChange={handleCheckboxChange}
                     />
                     <label
                       className="form-check-label text-light"
-                      htmlFor="flexCheckDefault"
+                      htmlFor="flexCheckDefault5"
                     >
-                      Korean
+                      Canadian
                     </label>
                   </div>
                   <div className="form-check">
                     <input
                       className="form-check-input"
                       type="checkbox"
-                      value=""
-                      id="flexCheckChecked"
+                      value="British"
+                      data-category="a"
+                      id="flexCheckDefault6"
+                      onChange={handleCheckboxChange}
                     />
                     <label
                       className="form-check-label text-light"
-                      htmlFor="flexCheckChecked"
+                      htmlFor="flexCheckDefault6"
                     >
                       British
                     </label>
@@ -133,12 +205,14 @@ function Recipepage() {
                     <input
                       className="form-check-input"
                       type="checkbox"
-                      value=""
-                      id="flexCheckDefault"
+                      value="American"
+                      data-category="a"
+                      id="flexCheckDefault7"
+                      onChange={handleCheckboxChange}
                     />
                     <label
                       className="form-check-label text-light"
-                      htmlFor="flexCheckDefault"
+                      htmlFor="flexCheckDefault7"
                     >
                       American
                     </label>
@@ -147,12 +221,14 @@ function Recipepage() {
                     <input
                       className="form-check-input"
                       type="checkbox"
-                      value=""
-                      id="flexCheckDefault"
+                      value="Japanese"
+                      data-category="a"
+                      id="flexCheckDefault8"
+                      onChange={handleCheckboxChange}
                     />
                     <label
                       className="form-check-label text-light"
-                      htmlFor="flexCheckDefault"
+                      htmlFor="flexCheckDefault8"
                     >
                       Japanese
                     </label>
@@ -162,6 +238,39 @@ function Recipepage() {
             </div>
           )}
         </div>
+        
+        {isSearching && filteredRecipes.length === 0 ? (
+          <p className="text-light">No recipes found.</p>
+        ) : (
+          filteredRecipes.length > 0 && (
+            <div className="food_items">
+              {filteredRecipes.map((recipe, index) => (
+                <div key={index} className="card foodies m-3">
+                  <img
+                    src={recipe.strMealThumb}
+                    className="card-img-top p-2"
+                    alt="..."
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">
+                      {isLoading ? (
+                        <div
+                          className="spinner-border text-dark"
+                          role="status"
+                        ></div>
+                      ) : (
+                        recipe.strMeal
+                      )}
+                    </h5>
+                    <a href="#" className="btn btn-primary">
+                      View Recipe
+                    </a>
+                  </div>
+                </div>
+              ))}
+            </div>
+          )
+        )}
       </section>
     </>
   );
