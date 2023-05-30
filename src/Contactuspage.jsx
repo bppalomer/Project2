a112dbd46b00a4e0396036352016ea70317c3ba2
Marjmainit <marjoriemainit.28@gmail.com>
2023-05-30 19:08:52 +0800

    fix contactpage

--- src/Contactuspage.jsx
+++ src/Contactuspage.jsx
@@ -2,12 +2,39 @@
 
 const Contactuspage = () => {
   return (
-    <div>
-        <h1>
-        This is is the contact us page
-        </h1>
-        
-        
+    <div className="mb-5 mt-5">
+     <div>
+     <h1 className="contact-head">
+        Contact Us
+      </h1>
+      <p className="intro">
+        We'd love to hear from you! If you have any questions, suggestions, or simply want to say hello, feel free to get in touch with us. 
+        Please fill out the form below, and we'll get back to you as soon as possible.
+      </p>
+     </div>
+      <div className="container col-5 p-5" id="form">
+       <div className="row">
+       <div className="mb-3">
+          <label className="form-label ">First Name</label>
+          <input type="text" className="form-control" placeholder="" />
+        </div>
+        <div className="mb-3"> 
+          <label className="form-label ">Last Name</label>
+          <input type="text" className="form-control" placeholder="" />
+        </div>
+        <div className="mb-3">
+          <label className="form-label ">Email address</label>
+          <input type="text" className="form-control" placeholder="name@example.com" />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Comments</label>
+          <textarea className="form-control" id="text-area" rows="3"></textarea>
+        </div>
+        <div>
+          <button type="button" class="btn btn-dark">Enter</button>
+        </div>
+      </div>
+       </div>
     </div>
   )
 }
