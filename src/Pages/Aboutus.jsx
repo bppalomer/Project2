3eea54a4e6e1d436c47d1ea9d5a7f1365399842e
Patrick <dalepatrick005@gmail.com>
2023-05-31 17:06:28 +0800

    change class into className

--- src/Pages/Aboutus.jsx
+++ src/Pages/Aboutus.jsx
@@ -2,36 +2,36 @@
 function Aboutus () {
     return (
         <section className="m-5">
-            <div class="row creators p-5 m-5 rounded">
-            <div class="col-md-6">
+            <div className="row creators p-5 m-5 rounded">
+            <div className="col-md-6">
                     <img src="#" className="m-4" alt="#"/>
                 </div>
-                <div class="col-md-6">
-                    <p class="fw-bold">Bryan Paul Palomer</p>
+                <div className="col-md-6">
+                    <p className="fw-bold">Bryan Paul Palomer</p>
                     <p>Full-Stack Developer</p>
                 </div>
                 
             </div>
 
 
-            <div class="row creators p-5 m-5 rounded">
-                <div class="col-md-6">
-                    <p class="fw-bold">Elliezer Layug</p>
+            <div className="row creators p-5 m-5 rounded">
+                <div className="col-md-6">
+                    <p className="fw-bold">Elliezer Layug</p>
                     <p>Full-Stack Developer</p>
                 </div>
-                <div class="col-md-6">
+                <div className="col-md-6">
                     <img src="#" className="m-4" alt="#"/>
                 </div>
             </div>
 
 
 
-            <div class="row creators p-5 m-5 rounded">
-            <div class="col-md-6">
+            <div className="row creators p-5 m-5 rounded">
+            <div className="col-md-6">
                     <img src="/pic.jpg" className="m-4" alt="#"/>
                 </div>
-                <div class="col-md-6">
-                    <p class="fw-bold">Patrick Laguna</p>
+                <div className="col-md-6">
+                    <p className="fw-bold">Patrick Laguna</p>
                     <p>Full-Stack Developer</p>
                 </div>
                 
@@ -44,7 +44,7 @@ function Aboutus () {
                     <p className="fw-bold">Marjori Mainit</p>
                     <p>Full-Stack Developer</p>
                 </div>
-                <div class="col-md-6">
+                <div className="col-md-6">
                     <img src="#" className="p-5" alt="#"/>
                 </div>
             </div>
