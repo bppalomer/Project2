116603c879ac46c2ee432192859f49ec5af070d4
Patrick <dalepatrick005@gmail.com>
2023-05-31 17:00:37 +0800

    added css and padding

--- src/Pages/Aboutus.jsx
+++ src/Pages/Aboutus.jsx
@@ -1,44 +1,51 @@
 
 function Aboutus () {
     return (
-        <section>
-            <div class="row" id="section">
-                <div class="col-md-6 p-5">
-                    <img src="pic.jpg" className="m-4" alt="pic.jpg"/>
+        <section className="m-5">
+            <div class="row creators p-5 m-5 rounded">
+            <div class="col-md-6">
+                    <img src="#" className="m-4" alt="#"/>
                 </div>
-                <div class="col-md-6 container-fluid p-5">
-                    <p class="fw-bold">Marites Sulayman</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti maxime porro placeat optio
-                        repellendus, eius expedita fugiat quibusdam assumenda saepe quia libero dicta dolores doloremque
-                        error obcaecati totam necessitatibus itaque.</p>
+                <div class="col-md-6">
+                    <p class="fw-bold">Bryan Paul Palomer</p>
+                    <p>Full-Stack Developer</p>
                 </div>
+                
             </div>
 
-            <br />
 
-            <div class="row" id="section_2">
-                <div class="col-md-6 container-fluid p-5">
-                    <p class="fw-bold">Baldy dulingan</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti maxime porro placeat optio
-                        repellendus, eius expedita fugiat quibusdam assumenda saepe quia libero dicta dolores doloremque
-                        error obcaecati totam necessitatibus itaque.</p>
+            <div class="row creators p-5 m-5 rounded">
+                <div class="col-md-6">
+                    <p class="fw-bold">Elliezer Layug</p>
+                    <p>Full-Stack Developer</p>
                 </div>
-                <div class="col-md-6 container-fluid p-5">
+                <div class="col-md-6">
                     <img src="#" className="m-4" alt="#"/>
                 </div>
             </div>
 
-            <br />
 
-            <div class="row" id="section_3">
-                <div class="col-md-6 container-fluid p-5">
-                    <img src="#" className="m-5" alt="#" />
+
+            <div class="row creators p-5 m-5 rounded">
+            <div class="col-md-6">
+                    <img src="/pic.jpg" className="m-4" alt="#"/>
                 </div>
-                <div class="col-md-6 container-fluid p-5">
-                    <p class="fw-bold">Marites Sulayman</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti maxime porro placeat optio
-                        repellendus, eius expedita fugiat quibusdam assumenda saepe quia libero dicta dolores doloremque
-                        error obcaecati totam necessitatibus itaque.</p>
+                <div class="col-md-6">
+                    <p class="fw-bold">Patrick Laguna</p>
+                    <p>Full-Stack Developer</p>
+                </div>
+                
+            </div>
+
+
+
+            <div className="row creators  p-5 m-5 rounded">
+                <div className="col-md-6">
+                    <p className="fw-bold">Marjori Mainit</p>
+                    <p>Full-Stack Developer</p>
+                </div>
+                <div class="col-md-6">
+                    <img src="#" className="p-5" alt="#"/>
                 </div>
             </div>
         </section>
