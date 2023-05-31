aab44fb3f7777ad277303f2c53294ff8e161844d
Patrick <dalepatrick005@gmail.com>
2023-05-31 16:59:44 +0800

    remove some padding and added pic, margin and css

--- src/Pages/Aboutus.jsx
+++ src/Pages/Aboutus.jsx
@@ -3,10 +3,10 @@ function Aboutus () {
     return (
         <section>
             <div class="row" id="section">
-                <div class="col-md-5 container-fluidp p-5 m-2">
-                    <img src="#" alt="#"/>
+                <div class="col-md-6 p-5">
+                    <img src="pic.jpg" className="m-4" alt="pic.jpg"/>
                 </div>
-                <div class="col-md-5 container-fluid p-3 m-5">
+                <div class="col-md-6 container-fluid p-5">
                     <p class="fw-bold">Marites Sulayman</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti maxime porro placeat optio
                         repellendus, eius expedita fugiat quibusdam assumenda saepe quia libero dicta dolores doloremque
@@ -17,24 +17,24 @@ function Aboutus () {
             <br />
 
             <div class="row" id="section_2">
-                <div class="col-md-5 container-fluid p-3 m-2">
+                <div class="col-md-6 container-fluid p-5">
                     <p class="fw-bold">Baldy dulingan</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti maxime porro placeat optio
                         repellendus, eius expedita fugiat quibusdam assumenda saepe quia libero dicta dolores doloremque
                         error obcaecati totam necessitatibus itaque.</p>
                 </div>
-                <div class="col-md-5 container-fluid p-5 m-2">
-                    <img src="#" alt="#"/>
+                <div class="col-md-6 container-fluid p-5">
+                    <img src="#" className="m-4" alt="#"/>
                 </div>
             </div>
 
             <br />
 
             <div class="row" id="section_3">
-                <div class="col-md-5 container-fluid p-3 m-2">
-                    <img src="#" alt="#" />
+                <div class="col-md-6 container-fluid p-5">
+                    <img src="#" className="m-5" alt="#" />
                 </div>
-                <div class="col-md-5 container-fluid p-3 m-5">
+                <div class="col-md-6 container-fluid p-5">
                     <p class="fw-bold">Marites Sulayman</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti maxime porro placeat optio
                         repellendus, eius expedita fugiat quibusdam assumenda saepe quia libero dicta dolores doloremque
