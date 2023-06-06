bb503966b589de3fbe7297dc3a09c3c26c72802c
xEliezer <layugeliezerbongco16@gmail.com>
2023-06-07 00:07:48 +0800

    refined the aos animations

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { useNavigate } from 'react-router-dom';
-=======
-import { Helmet } from "react-helmet";
->>>>>>> main
 
 function Home() {
 
@@ -12,42 +8,42 @@ function Home() {
         <>
             {/* Hero Section */}
             <section id="hero-section" className="container-fluid p-5 shadow-lg">
-            <Helmet>
-        <title>SpiceSavvy - Home </title>
-      </Helmet>
                 <div className="container">
-                    <div className="row d-flex justify-content-start align-items-center">
-                        <div className="col-md-5">
-                            <h1 className="fw-bold text-white display-4 mt-2" 
-                                data-aos="zoom-in" 
+                    <div className="row d-flex">
+                        <div className="col-md-6">
+                            <img id="cooking" className="img-fluid" src="/cooking.png" alt="image"
+                                data-aos="fade-right"
+                                data-aos-easing="ease-in-sine"
+                                data-aos-duration="1500" />
+                            <h1 className="fw-bold text-white display-4" 
+                                data-aos="zoom-in"
                                 data-aos-easing="ease-in-sine"
                                 data-aos-duration="1500">
                                 Discover
-                                <br />
                                 Delicious Recipes,
-                                <br />
                                 Just for You!
                             </h1>
-                            <h4 className="text-white" data-aos="zoom-in" 
-                                data-aos-easing="ease-in-sine"
-                                data-aos-duration="1500">
-                                Unleash your creativity in the kitchen 
-                                <br/>
-                                with unique and mouthwatering recipes.
-                            </h4>
-                            <button id="hero-btn" className="btn ps-5 pe-5 p-4 mt-4 shadow-lg fw-semibold" 
+                            <h4 className="text-white" 
                                 data-aos="zoom-in" 
                                 data-aos-easing="ease-in-sine"
-                                data-aos-duration="1500"
-                                onClick={() => navigate('/recipepage')}>
+                                data-aos-duration="1500">
+                                Unleash your creativity in the kitchen
+                                <br />
+                                with unique and mouthwatering recipes.
+                            </h4>
+                            <button onClick={() => navigate('/recipepage')} id="hero-btn" 
+                            className="btn ps-5 pe-5 p-4 mt-4 mb-5 shadow-lg fw-semibold"
+                                data-aos="fade-up"
+                                data-aos-easing="ease-in-sine"
+                                data-aos-duration="3000">
                                 Explore Recipes
                             </button>
                         </div>
-                        <div className="col-md-7 p-4">
-                            <img className="plate-1" src="/Plate1.png" alt="image" 
-                                data-aos="fade-left" 
+                        <div className="col-md-6 d-flex justify-content-start align-items-center">
+                            <img className="plate-1" src="/Plate1.png" alt="image"
+                                data-aos="fade-left"
                                 data-aos-easing="ease-in-sine"
-                                data-aos-duration="1500"/>
+                                data-aos-duration="1500" />
                         </div>
                     </div>
                 </div>
